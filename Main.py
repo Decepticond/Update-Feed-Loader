042412c6b3b469c8ee10122a51920b2615aaa03d
Decepticond <brookiecookiespink@gmail.com>
2022-05-02 19:08:37 -0400

    (Possible) Final loader
    Created an empty list to add to the dict in order for it to print nicely.

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+# Note: indent by 2 is for a better look.
 
 import requests
 import json
@@ -7,12 +8,17 @@ r = requests.get('https://stackoverflow.com/feeds/tag?tagnames=python&sort=newes
 
 selector = Selector(text=r.text, type='xml')
 selector.remove_namespaces()
-
 entries = selector.xpath('//entry')
+list_of_entries = [] 
+
 
 for entry in entries:
   dict = {
     'title': entry.xpath('title/text()').get(),
     'link': entry.xpath('link').attrib['href'],
   }
-  print(dict)
+  list_of_entries.append(dict)
+
+
+print(json.dumps(list_of_entries, indent=2))
+  
