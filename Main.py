4e6cb47a0858a2ea90308e2e0dc35ab668cb2e32
Decepticond <brookiecookiespink@gmail.com>
2022-05-02 18:05:29 -0400

    Creation of dict
    Upon creation of entries variable, creating a foor loop so it loops within the XML doc and finds the titles, and links

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+
 import requests
 import json
 from parsel import Selector
@@ -8,3 +9,10 @@ selector = Selector(text=r.text, type='xml')
 selector.remove_namespaces()
 
 entries = selector.xpath('//entry')
+
+for entry in entries:
+  dict = {
+    'title': entry.xpath('title/text()').get(),
+    'link': entry.xpath('link').attrib['href'],
+  }
+  print(dict)
