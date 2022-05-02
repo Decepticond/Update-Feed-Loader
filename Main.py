cd6807629285f26e281e2caeecf45f21c4810016
Decepticond <brookiecookiespink@gmail.com>
2022-05-02 17:14:45 -0400

    Using parsel now to request the URL
    Requesting XML feed and printing it to see if it'll give me it back w/o error.

--- Main.py
+++ Main.py
@@ -1,19 +1,6 @@
+import requests
 import json
-import feedparser
+from parsel import Selector
 
-url = "https://stackoverflow.com/feeds/tag?tagnames=python&sort=newest"
-dir(feedparser)
-f = feedparser.parse(url)
-
-
-"""
-print("hi")
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
-
-    return {"statusCode": 200, "body": json.dumps(body)}
-"""
-#test
+r = requests.get('https://stackoverflow.com/feeds/tag?tagnames=python&sort=newest')
+print (r.text) 
