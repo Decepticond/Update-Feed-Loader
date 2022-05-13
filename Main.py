b5e1a1ee29a1cfe9c61c85eb46f0e8dd865dccb8
Decepticond <brookiecookiespink@gmail.com>
2022-05-12 22:24:56 -0400

    Trying to resolve issue
    Making it a function for eases sake - trying to make it importable so that I can work around the issue of AWS being a pain about letting me use custom modules.

--- Main.py
+++ Main.py
@@ -4,21 +4,24 @@ import requests
 import json
 from parsel import Selector
 
-r = requests.get('https://stackoverflow.com/feeds/tag?tagnames=python&sort=newest')
 
-selector = Selector(text=r.text, type='xml')
-selector.remove_namespaces()
-entries = selector.xpath('//entry')
-list_of_entries = [] 
+def main():
+  
+  r = requests.get('https://stackoverflow.com/feeds/tag?tagnames=python&sort=newest')
+  selector = Selector(text=r.text, type='xml')
+  selector.remove_namespaces()
+  entries = selector.xpath('//entry')
+  list_of_entries = [] 
 
-
-for entry in entries:
-  dict = {
+  for entry in entries:
+    dict = {
     'title': entry.xpath('title/text()').get(),
     'link': entry.xpath('link').attrib['href'],
   }
   list_of_entries.append(dict)
 
 
-print(json.dumps(list_of_entries, indent=2))
-  
+  print(json.dumps(list_of_entries, indent=2))
+
+if __name__ == '__main__':
+  main()
